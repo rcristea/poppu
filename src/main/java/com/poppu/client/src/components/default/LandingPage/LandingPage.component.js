a4b17e20b1972c046c85de757d7ecc8b482652d6
rubencristea <rubendcristea@gmail.com>
2021-08-29 10:10:14 -0400

    Added cool landing page background and div border

--- src/main/java/com/poppu/client/src/components/default/LandingPage/LandingPage.component.js
+++ src/main/java/com/poppu/client/src/components/default/LandingPage/LandingPage.component.js
@@ -9,6 +9,15 @@ export class LandingPage extends Component {
         <NavBar />
         <div className='landing-page-container'>
           <div className='landing-page-hero'>
+            <div className='background'></div>
+            <div className="custom-shape-divider-bottom-1630245566">
+              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                   preserveAspectRatio="none">
+                <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" className="shape-fill"></path>
+              </svg>
+            </div>
+          </div>
+          <div className='test'>
 
           </div>
         </div>
