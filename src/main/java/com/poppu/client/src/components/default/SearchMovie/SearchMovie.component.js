cf4d486c52cc31cef85b7d9c0ee91c85a7d9af4a
rubencristea <rubendcristea@gmail.com>
2021-09-17 22:09:52 -0400

    Added category and date filters to search

--- src/main/java/com/poppu/client/src/components/default/SearchMovie/SearchMovie.component.js
+++ src/main/java/com/poppu/client/src/components/default/SearchMovie/SearchMovie.component.js
@@ -9,10 +9,26 @@ export class SearchMovie extends Component {
       <div className='search-movie-container'>
         <form className='search-movie-form'>
           <div className='query-container'>
-            <input type='text' className='query' placeholder='Search movies'/>
+            <input type='text' className='query' placeholder='Search Movies'/>
             <button type="submit" className="searchButton">
-              <AiOutlineSearch size='32px'/>
+              <AiOutlineSearch size='24px'/>
             </button>
+            <div className='category-container'>
+              <label>
+                Category
+              </label>
+              <select>
+                <option value='01'>Category 01</option>
+                <option value='02'>Category 02</option>
+                <option value='03'>Category 03</option>
+              </select>
+            </div>
+            <div className='date-container'>
+              <label>
+                Show Date
+              </label>
+              <input type='date' />
+            </div>
           </div>
         </form>
       </div>
