731a3b4a37facf57a77c82ab1aabb71ae7881976
rubencristea <rubendcristea@gmail.com>
2021-09-18 09:06:37 -0400

    Made SearchMovie responsive

--- src/main/java/com/poppu/client/src/components/default/SearchMovie/SearchMovie.component.js
+++ src/main/java/com/poppu/client/src/components/default/SearchMovie/SearchMovie.component.js
@@ -1,9 +1,16 @@
-import { Component } from 'react'
+import {Component, createRef} from 'react'
 import './SearchMovie.component.css'
 
 import { AiOutlineSearch } from 'react-icons/ai'
 
 export class SearchMovie extends Component {
+  constructor(props) {
+    super(props)
+
+    this.today = new Date()
+    this.today = this.today.toISOString().substr(0, 10)
+  }
+
   render() {
     return (
       <div className='search-movie-container'>
@@ -13,22 +20,22 @@ export class SearchMovie extends Component {
             <button type="submit" className="searchButton">
               <AiOutlineSearch size='24px'/>
             </button>
-            <div className='category-container'>
-              <label>
-                Category
-              </label>
-              <select>
-                <option value='01'>Category 01</option>
-                <option value='02'>Category 02</option>
-                <option value='03'>Category 03</option>
-              </select>
-            </div>
-            <div className='date-container'>
-              <label>
-                Show Date
-              </label>
-              <input type='date' />
-            </div>
+          </div>
+          <div className='category-container'>
+            <label>
+              Category
+            </label>
+            <select>
+              <option value='01'>Category 01</option>
+              <option value='02'>Category 02</option>
+              <option value='03'>Category 03</option>
+            </select>
+          </div>
+          <div className='date-container'>
+            <label>
+              Show Date
+            </label>
+            <input type='date' defaultValue={this.today}/>
           </div>
         </form>
       </div>
