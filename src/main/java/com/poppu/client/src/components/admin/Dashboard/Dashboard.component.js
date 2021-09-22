f7e2fadead323949f628ff73ec389c61cf7fe73f
rubencristea <rubendcristea@gmail.com>
2021-09-22 12:56:24 -0400

    Added dashboard component

---
+++ src/main/java/com/poppu/client/src/components/admin/Dashboard/Dashboard.component.js
@@ -0,0 +1,14 @@
+import { React, Component } from 'react'
+import './Dashboard.component.css'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    )
+  }
+}
+
+export default Dashboard
