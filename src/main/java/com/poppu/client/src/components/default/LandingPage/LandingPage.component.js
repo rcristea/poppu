08c8c0d77b28653947d57190299b85a7f63fe657
rubencristea <rubendcristea@gmail.com>
2021-10-06 13:56:36 -0400

    Replaced youtube iframe to movie posters

--- src/main/java/com/poppu/client/src/components/default/LandingPage/LandingPage.component.js
+++ src/main/java/com/poppu/client/src/components/default/LandingPage/LandingPage.component.js
@@ -10,37 +10,43 @@ export class LandingPage extends Component {
       'movie_id': 1,
       'movie_name': 'Shang-Chi',
       'movie_rating': 8.8,
-      'movie_trailer_link': '8YjFbMbfXaQ'
+      'movie_trailer_link': '8YjFbMbfXaQ',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 2,
       'movie_name': 'Uncle Frank',
       'movie_rating': 7.8,
-      'movie_trailer_link': 'K8nm0iYLvcs'
+      'movie_trailer_link': 'K8nm0iYLvcs',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 3,
       'movie_name': 'KATE',
       'movie_rating': 6.6,
-      'movie_trailer_link': 'MysGjRS9jFU'
+      'movie_trailer_link': 'MysGjRS9jFU',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 4,
       'movie_name': 'Don\'t Breathe 2',
       'movie_rating': 8.2,
-      'movie_trailer_link': 'gRbG2tjHYCA'
+      'movie_trailer_link': 'gRbG2tjHYCA',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 5,
       'movie_name': 'Cinderella',
       'movie_rating': 1.5,
-      'movie_trailer_link': '20DF6U1HcGQ'
+      'movie_trailer_link': '20DF6U1HcGQ',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 6,
       'movie_name': 'Red Dot',
       'movie_rating': 5.7,
-      'movie_trailer_link': 't7FwypV69qc'
+      'movie_trailer_link': 't7FwypV69qc',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
   ]
   comingSoon = [
@@ -48,37 +54,43 @@ export class LandingPage extends Component {
       'movie_id': 7,
       'movie_name': 'After We Fell',
       'movie_rating': 5.4,
-      'movie_trailer_link': 'NYdNN6C9hfI'
+      'movie_trailer_link': 'NYdNN6C9hfI',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 8,
       'movie_name': 'SPIDER-MAN: NO WAY HOME',
       'movie_rating': 7.8,
-      'movie_trailer_link': 'rt-2cxAiPJk'
+      'movie_trailer_link': 'rt-2cxAiPJk',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 9,
       'movie_name': 'My Hero Academia: World Heroes\' Mission',
       'movie_rating': 6.6,
-      'movie_trailer_link': '6cBYUfAno-0'
+      'movie_trailer_link': '6cBYUfAno-0',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 10,
       'movie_name': 'The Matrix Ressurections',
       'movie_rating': 4.2,
-      'movie_trailer_link': '9ix7TUGVYIo'
+      'movie_trailer_link': '9ix7TUGVYIo',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 11,
       'movie_name': 'Eternals',
       'movie_rating': 7.5,
-      'movie_trailer_link': 'x_me3xsvDgk'
+      'movie_trailer_link': 'x_me3xsvDgk',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
     {
       'movie_id': 12,
       'movie_name': 'SPIDER-MAN: INTO THE SPIDER-VERSE 2',
       'movie_rating': 9.7,
-      'movie_trailer_link': 'HsX8pVqp_gg'
+      'movie_trailer_link': 'HsX8pVqp_gg',
+      'movie_poster': 'assets/img/posters/shangchi_and_the_legend_of_the_ten_rings.jpeg',
     },
   ]
 
