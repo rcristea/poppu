65f10b11bc3913eae4e61ba2fd46ebb39eb4c0d0
AbhinavSingh2000 <89401630+AbhinavSingh2000@users.noreply.github.com>
2021-10-20 10:21:59 -0400

    Added validator stuff

--- src/main/java/com/poppu/server/model/ValidatorModel.java
+++ src/main/java/com/poppu/server/model/ValidatorModel.java
@@ -1,6 +1,62 @@
 package com.poppu.server.model;
 
+import javax.persistence.*;
+import java.util.Random;
 
+@Entity
+@Table(name = "validator")
 public class ValidatorModel {
 
+    @Id
+    @Column (name = "email", nullable = false)
+    private String email;
+
+    @Column (name = "code", nullable = false)
+    private int code;
+
+    @Column(name = "validated", columnDefinition = "tinyint(1) default 0")
+    private boolean validated;
+
+    public ValidatorModel() {
+
+    }
+
+    public ValidatorModel(String email) {
+        this.email = email;
+        this.code = genCode(1000000);
+        this.validated = false;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public boolean isValidated() {
+        return validated;
+    }
+
+    public void setValidated(boolean validated) {
+        this.validated = validated;
+    }
+
+    private int genCode(int upperBound) {
+        Random rand = new Random();
+        return rand.nextInt(upperBound);
+    }
+
+    public void sendEmail() {
+
+    }
 }
