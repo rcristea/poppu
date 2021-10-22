939e952b9236a79066959876e714b717264d221e
rubencristea <rubendcristea@gmail.com>
2021-10-22 12:32:24 -0400

    Added cross origin from :3000

--- src/main/java/com/poppu/server/repository/UserRepository.java
+++ src/main/java/com/poppu/server/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.poppu.server.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     UserModel findDistinctByEmail(@Param("email") String email);
--- src/main/java/com/poppu/server/repository/ValidatorRepository.java
+++ src/main/java/com/poppu/server/repository/ValidatorRepository.java
@@ -3,7 +3,9 @@ package com.poppu.server.repository;
 import com.poppu.server.model.ValidatorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(collectionResourceRel = "validators", path = "validators")
 public interface ValidatorRepository extends JpaRepository<ValidatorModel, String> {
 
