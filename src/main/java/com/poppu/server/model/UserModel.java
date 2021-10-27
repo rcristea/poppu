66d0c9f3753d2d0362cf1918644206b12a83eb2f
rubencristea <rubendcristea@gmail.com>
2021-10-27 12:07:19 -0400

    @AbhinavSingh2000 Fixed our problem

--- src/main/java/com/poppu/server/model/UserModel.java
+++ src/main/java/com/poppu/server/model/UserModel.java
@@ -44,7 +44,7 @@ public class UserModel {
     @Column(name = "status", columnDefinition = "varchar(10) default 'ACTIVE'")
     private Status status = Status.ACTIVE;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", unique = true)
     private AddressModel address;
 
