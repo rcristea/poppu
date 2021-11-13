a3527426fd96530d758a125197a6b8e14244f0ea
EileenTheCrow <eileencatoe@gmail.com>
2021-11-12 21:23:13 -0500

    Added Suspend/Unsuspend button.

--- src/main/java/com/poppu/client/src/components/admin/Users/Index/UserList.component.js
+++ src/main/java/com/poppu/client/src/components/admin/Users/Index/UserList.component.js
@@ -4,19 +4,54 @@ import DropdownToggle from "react-bootstrap/DropdownToggle";
 import {BiDotsVerticalRounded} from "react-icons/bi";
 import DropdownMenu from "react-bootstrap/DropdownMenu";
 import DropdownItem from "react-bootstrap/DropdownItem";
-import { Container, Table } from 'reactstrap';
+import {Button, Container, Table} from 'reactstrap';
 
 
 class UserList extends Component{
     constructor(props) {
         super(props);
         this.state = {users: []};
+
+        this.toggleSuspendUser = this.toggleSuspendUser.bind(this);
     }
 
     componentDidMount() {
         fetch('/api/users/')
             .then(response => response.json())
             .then(data => this.setState({users: data}));
+    }
+
+    async toggleSuspendUser(userId, shouldSuspend) {
+        const users = this.state.users;
+        const targetUserIndex = users.findIndex(user => user.id === userId)
+        users[targetUserIndex].status = shouldSuspend ? 'SUSPENDED' : 'ACTIVE';
+
+        await fetch(`/api/users/${users[targetUserIndex].id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(users[targetUserIndex]),
+        });
+        this.setState({ users });
+    }
+
+
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+
+        await fetch('/api/users' + ('/' + item.id), {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        this.props.history.push('/users');
     }
 
     render() {
@@ -31,6 +66,14 @@ class UserList extends Component{
                 <td>{user.isSubscribed ? 'Yes' : 'No'}</td>
                 <td>{user.role}</td>
                 <td>{user.status}</td>
+                <td>
+                    <Button type="button"
+                            name="status"
+                            id="status"
+                            onClick={() => this.toggleSuspendUser(user.id, user.status === 'ACTIVE')}>
+                        {user.status === "ACTIVE" ? "SUSPEND" : "ACTIVE"}
+                    </Button>
+                </td>
                 <td>
                     <Dropdown className='card-table-dropdown'>
                         <DropdownToggle
@@ -54,11 +97,12 @@ class UserList extends Component{
                             <th width="5%">ID</th>
                             <th width="15%">First Name</th>
                             <th width="15%">Last Name</th>
-                            <th width="15%">Email</th>
-                            <th width="15%">Phone Number</th>
+                            <th width="10%">Email</th>
+                            <th width="10%">Phone Number</th>
                             <th width="5%">Subscribed</th>
                             <th width="15%">Role</th>
                             <th width="15%">Status</th>
+                            <th width="10&"></th>
                         </tr>
                         </thead>
                         <tbody>
