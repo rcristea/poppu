bfc4d3deaa87346b6b9891bcb00c8c71cff892e5
TimWargo <60803886+TimWargo@users.noreply.github.com>
2021-11-14 16:06:14 -0500

    Got some of the data to show. Still need to work out the problems

--- src/main/java/com/poppu/client/src/components/admin/Movies/Index/ManageMovies.component.js
+++ src/main/java/com/poppu/client/src/components/admin/Movies/Index/ManageMovies.component.js
@@ -6,6 +6,63 @@ import AutoTable from '../../../utils/AutoTable.component'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export class ManageMoviesComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [{
+          id: null,
+          movie_name: "",
+          movie_rating: "",
+          movie_ratingCode: "",
+          movie_isShowing: "",
+        },]
+    }
+    this.getMovies = this.getMovies.bind(this)
+  }
+
+  getMovies() {
+    return fetch(`http://localhost:8080/movies/`, {
+      method: 'GET',
+    }).then(response => {
+      if (response.ok) {
+        return response.json().then(json => {
+          console.log('getMovies', json._embedded.movies)
+          return json._embedded.movies
+        })
+      } else {
+        console.error('getMovies', response)
+        return null
+      }
+    }).catch(error => {
+      console.error('getMovies', error)
+      return null
+    })
+  }
+
+  async componentDidMount() {
+    let movies = await this.getMovies()
+    let formattedMovies = []
+    console.log(movies[1])
+    movies.forEach(movie => {
+      formattedMovies.push({
+        id: movie.movie_id,
+        movieName: movie.title,
+        movieRating: movie.score,
+        movieRatingCode: movie.rating,
+        movieIsShowing: movie.isShowing,
+      })
+    })
+    formattedMovies.slice(1, formattedMovies.length)
+    this.setState({
+      movies: formattedMovies,
+    })
+    console.log(formattedMovies)
+  }
+
+  formatMovies() {
+
+  }
   movies = [
     {
       'id': 1,
@@ -101,9 +158,9 @@ export class ManageMoviesComponent extends Component {
         </Row>
         <AutoTable
           card_title={'Movies'}
-          headings={'id,movie_name,movie_rating,movie_trailer_link,showtime,'}
+          headings={'id,movie_name,movie_rating,movie_rating_code,status,'}
           widths={[1, 5, 1, 1, 2]}
-          table_data={this.movies}
+          table_data={this.state.movies}
           viewable={true}
           editable={true}
           deletable={true}
