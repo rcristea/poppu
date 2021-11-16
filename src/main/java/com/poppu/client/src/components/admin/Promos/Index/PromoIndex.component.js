c03e36dbb390737be058657bf1a377583a0e30ee
Basu <tridibb@hotmail.com>
2021-11-15 21:03:35 -0500

    Implemented PromoIndex component to get and delete data from the promotions table.

--- src/main/java/com/poppu/client/src/components/admin/Promos/Index/PromoIndex.component.js
+++ src/main/java/com/poppu/client/src/components/admin/Promos/Index/PromoIndex.component.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {React, Component} from 'react'
 import './PromoIndex.component.css'
 import Sidebar from '../../Sidebar/Sidebar.component'
@@ -9,12 +8,14 @@ import {Dropdown, Table} from 'react-bootstrap'
 import DropdownToggle from 'react-bootstrap/DropdownToggle'
 import DropdownMenu from 'react-bootstrap/DropdownMenu'
 import DropdownItem from 'react-bootstrap/DropdownItem'
+import AutoTable from "../../../utils/AutoTable.component";
 
 class PromoIndex extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const promos = [
+    /* Testing data for promos. TODO: set up promo indexing from database -> frontend */
+    /*const promos = [
       {
         'id': 1,
         'code': 'one',
@@ -86,21 +87,52 @@ class PromoIndex extends Component {
         'start_date': '2022-01-01T00:00:00',
         'end_date': '2022-01-02T00:00:00',
       }
+
+
     ]
-
+     */
+    /* Matching the state of the indexer to the shape of the promotions table. */
     this.state = {
-      promos: promos,
+      promotions: [{
+        promotion_id: "", //bigint
+        end_time: "", //datetime
+        is_sent: false, //tinyint(1)
+        offer: "", //varchar(255)
+        start_time: "", //datetime
+      },]
     }
-
+    /* Binds methods to the class */
     this.handleDelete = this.handleDelete.bind(this)
     this.logOut = this.logOut.bind(this)
     this.renderAlert = this.renderAlert.bind(this)
+    this.getPromotions = this.getPromotions.bind(this)
+  }
+
+  getPromotions() {
+    return fetch('http://localhost:8080/promotions/',  {
+      method: 'GET',
+    }).then(response => {
+      if (response.ok) {
+        return response.json().then(json => {
+          console.log('getPromotions', json._embedded.promotions)
+          return json._embedded.promotions
+        })
+      } else {
+        console.error('getPromotions', response)
+        return null
+      }
+    }).catch(error => {
+      console.error('getPromotions', error)
+      return null
+    })
   }
 
   handleDelete(id) {
     return e => {
       e.preventDefault()
-      alert('Attempted to delete promo with id: ' + id)
+      fetch('http://localhost:8080/promotions/'+id, { method: 'DELETE' })
+          .then(() => this.setState({ status: 'Delete successful' }))
+          .then(window.location.reload(false))
     }
   }
 
@@ -140,12 +172,32 @@ class PromoIndex extends Component {
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     if (sessionStorage.getItem('role') !== 'admin') {
       sessionStorage.setItem('alert', 'User does not have correct privileges.')
       this.props.history.push('/')
+    } else {
+      let promotions = await this.getPromotions()
+      let formattedPromotions = []
+      console.log(promotions[0])
+      promotions.forEach(promotion => {
+        formattedPromotions.push({
+          promotionId: promotion.promotionId,
+          endTime: new Date(promotion.endTime).toLocaleString('en-US'),
+          isSent: promotion.isSent,
+          offer: promotion.offer,
+          startTime: new Date(promotion.startTime).toLocaleString('en-US'),
+        })
+      })
+      //formattedPromotions.slice(1, formattedPromotions.length)
+      this.setState({
+        promotions: formattedPromotions,
+      })
+      console.log(formattedPromotions)
     }
   }
+
+
 
   render() {
     return (
@@ -169,37 +221,41 @@ class PromoIndex extends Component {
                 <Table responsive bordered className='promos-card-table text-black'>
                   <thead>
                   <tr>
-                    <th>ID</th>
-                    <th>Promo Code</th>
-                    <th>Amount</th>
+                    <th>Promotion ID</th>
+                    <th>Offer</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
                     <th></th>
                   </tr>
                   </thead>
                   <tbody>
-                  {this.state.promos.map(promo => (
-                    <tr key={promo.id}>
-                      <td>{promo.id}</td>
-                      <td>{promo.code}</td>
-                      <td>{promo.amount}</td>
-                      <td>
-                        <Dropdown className='card-table-dropdown'>
-                          <DropdownToggle
-                            className='card-table-dropdown-button'><BiDotsVerticalRounded/></DropdownToggle>
 
-                          <DropdownMenu>
-                            <DropdownItem href={`/promos/${promo.id}`}>View</DropdownItem>
-                            <DropdownItem href={`/promos/edit/${promo.id}`}>Edit</DropdownItem>
-                            <Dropdown.Divider/>
-                            <form onSubmit={this.handleDelete(promo.id)}>
-                              <button className='delete-promo' type='submit'>Delete</button>
-                            </form>
-                          </DropdownMenu>
-                        </Dropdown>
-                      </td>
-                    </tr>
+                  {this.state.promotions.map(promo => (
+                      <tr key={promo.promotionId}>
+                        <td>{promo.promotionId}</td>
+                        <td>{promo.offer}</td>
+                        <td>{promo.startTime}</td>
+                        <td>{promo.endTime}</td>
+                        <td>
+                          <Dropdown className='card-table-dropdown'>
+                            <DropdownToggle
+                                className='card-table-dropdown-button'><BiDotsVerticalRounded/></DropdownToggle>
+
+                            <DropdownMenu>
+                              <DropdownItem href={`/promos/${promo.promotionId}`}>View</DropdownItem>
+                              <DropdownItem href={`/promos/edit/${promo.promotionId}`}>Edit</DropdownItem>
+                              <Dropdown.Divider/>
+                              <form onSubmit={this.handleDelete(promo.promotionId)}>
+                                <button className='delete-promo' type='submit'>Delete</button>
+                              </form>
+                            </DropdownMenu>
+                          </Dropdown>
+                        </td>
+                      </tr>
                   ))}
                   </tbody>
                 </Table>
+
               </div>
             </div>
           </div>
@@ -208,176 +264,4 @@ class PromoIndex extends Component {
     )
   }
 }
-
-=======
-import {React, Component} from 'react'
-import './PromoIndex.component.css'
-import Sidebar from '../../Sidebar/Sidebar.component'
-import SearchBar from '../SearchBar/SearchBar.component'
-import {BiDotsVerticalRounded, BiPlus} from 'react-icons/bi'
-import {Dropdown, Table} from 'react-bootstrap'
-import DropdownToggle from 'react-bootstrap/DropdownToggle'
-import DropdownMenu from 'react-bootstrap/DropdownMenu'
-import DropdownItem from 'react-bootstrap/DropdownItem'
-
-class PromoIndex extends Component {
-  constructor(props) {
-    super(props)
-
-    const promos = [
-      {
-        'id': 1,
-        'code': 'one',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 2,
-        'code': 'two',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 3,
-        'code': 'three',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      }
-      ,
-      {
-        'id': 4,
-        'code': 'four',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 5,
-        'code': 'five',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 6,
-        'code': 'six',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 7,
-        'code': 'seven',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 8,
-        'code': 'eight',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 9,
-        'code': 'nine',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      },
-      {
-        'id': 10,
-        'code': 'ten',
-        'amount': 0,
-        'start_date': '2022-01-01T00:00:00',
-        'end_date': '2022-01-02T00:00:00',
-      }
-    ]
-
-    this.state = {
-      promos: promos,
-    }
-
-    this.handleDelete = this.handleDelete.bind(this)
-  }
-
-  handleDelete(id) {
-    return e => {
-      e.preventDefault()
-      alert('Attempted to delete promo with id: ' + id)
-    }
-  }
-
-  componentDidMount() {
-    if (sessionStorage.getItem('role') !== 'admin') {
-      sessionStorage.setItem('alert', 'User does not have correct privileges.')
-      this.props.history.push('/')
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <Sidebar />
-        <div className='promos-container'>
-          <div className='cover'>
-            <div className='promos-card'>
-              <div className='promos-card-heading'>
-                <div className='promos-card-title'>
-                  <h1>Promotions</h1>
-                  <a href='/promos/add'>Add <BiPlus/></a>
-                </div>
-                <div className='promos-card-subtitle'>
-                  <h3>For more options, click the three dots on the right.</h3>
-                </div>
-              </div>
-              <div className='promos-card-content'>
-                <SearchBar/>
-                <Table responsive bordered className='promos-card-table text-black'>
-                  <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Promo Code</th>
-                    <th>Amount</th>
-                    <th></th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {this.state.promos.map(promo => (
-                    <tr key={promo.id}>
-                      <td>{promo.id}</td>
-                      <td>{promo.code}</td>
-                      <td>{promo.amount}</td>
-                      <td>
-                        <Dropdown className='card-table-dropdown'>
-                          <DropdownToggle
-                            className='card-table-dropdown-button'><BiDotsVerticalRounded/></DropdownToggle>
-                          <DropdownMenu>
-                            <DropdownItem href={`/promos/${promo.id}`}>View</DropdownItem>
-                            <DropdownItem href={`/promos/edit/${promo.id}`}>Edit</DropdownItem>
-                            <Dropdown.Divider/>
-                            <form onSubmit={this.handleDelete(promo.id)}>
-                              <button className='delete-promo' type='submit'>Delete</button>
-                            </form>
-                          </DropdownMenu>
-                        </Dropdown>
-                      </td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </Table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    )
-  }
-}
-
->>>>>>> development
 export default PromoIndex
