e4d8b2f891dd01b79362b5fa59e0aa30a66e8e50
rubencristea <rubendcristea@gmail.com>
2021-11-15 19:14:05 -0500

    Finished movie scheduling index

--- src/main/java/com/poppu/client/src/components/admin/Schedules/Index/Schedules.component.js
+++ src/main/java/com/poppu/client/src/components/admin/Schedules/Index/Schedules.component.js
@@ -14,14 +14,104 @@ class Schedule extends Component {
     this.state = {
       shows: null,
     }
+
+    this.getShows = this.getShows.bind(this)
+    this.getEndTime = this.getEndTime.bind(this)
+    this.renderShows = this.renderShows.bind(this)
   }
 
-  componentDidMount() {
+  getEndTime(dateTime, duration) {
+    let startTime = new Date(dateTime)
+    let durationHours = parseInt(duration.substring(0,1))
+    let durationMinutes = parseInt(duration.substring(3,5))
+    let endTime = new Date(startTime)
+    endTime.setHours(endTime.getHours() + durationHours)
+    endTime.setMinutes(endTime.getMinutes() + durationMinutes)
+
+    return endTime.toString()
+  }
+
+  getMovie(id) {
+    return fetch(`http://localhost:8080/shows/${id}/movie`, {
+      method: 'GET',
+    }).then(response => {
+      if (response.ok) {
+        return response.json().then(json => {
+          return json
+        })
+      } else {
+        return null
+      }
+    }).catch(error => {
+      return null
+    })
+  }
+
+  getShows() {
+    return fetch(`http://localhost:8080/shows`, {
+      method: 'GET',
+    }).then(response => {
+      if (response.ok) {
+        return response.json().then(async(json) => {
+          let shows = json._embedded.shows
+          console.log('getShows', shows)
+          return shows
+        })
+      } else {
+        console.error('getShows', response)
+        return null
+      }
+    }).catch(error => {
+      console.error('getShows', error)
+      return null
+    })
+  }
+
+  renderShows() {
+    if (this.state.shows) {
+      return this.state.shows.map(show => {
+        let startTime = new Date(show.dateTime)
+        let endTime = new Date(this.getEndTime(show.dateTime, show.movie.duration))
+
+        return (
+          <tr key={show.showID}>
+            <td>{show.movie.title}</td>
+            <td>{show.showroom.showroomId}</td>
+            <td>{startTime.toLocaleString("en-US")}</td>
+            <td>{endTime.toLocaleString("en-US")}</td>
+            <td>
+              <Dropdown className='card-table-dropdown'>
+                <DropdownToggle
+                  className='card-table-dropdown-button'><BiDotsVerticalRounded/></DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem href={`/`}>TODO</DropdownItem>
+                  <DropdownItem href={`/`}>TODO</DropdownItem>
+                  <Dropdown.Divider/>
+                  <form onSubmit={null}>TODO
+                    <button className='delete-schedule' type='submit'>TODO</button>
+                  </form>
+                </DropdownMenu>
+              </Dropdown>
+            </td>
+          </tr>
+        )
+      })
+    }
+  }
+
+  async componentDidMount() {
     if (sessionStorage.getItem('role') !== 'admin') {
       sessionStorage.setItem('alert', 'User does not have correct privileges.')
       this.props.history.push('/')
     }
 
+    let shows = await this.getShows()
+
+    console.log('componentDidMount', shows)
+
+    this.setState({
+      shows: shows
+    })
 
   }
 
@@ -53,27 +143,7 @@ class Schedule extends Component {
                   </tr>
                   </thead>
                   <tbody>
-                  {/*{this.state.movies.map(movie => (*/}
-                  {/*  <tr key={null}>TODO*/}
-                  {/*    <td>TODO</td>*/}
-                  {/*    <td>TODO</td>*/}
-                  {/*    <td>TODO</td>*/}
-                  {/*    <td>*/}
-                  {/*      <Dropdown className='card-table-dropdown'>*/}
-                  {/*        <DropdownToggle*/}
-                  {/*          className='card-table-dropdown-button'><BiDotsVerticalRounded/></DropdownToggle>*/}
-                  {/*        <DropdownMenu>*/}
-                  {/*          <DropdownItem href={`/`}>TODO</DropdownItem>*/}
-                  {/*          <DropdownItem href={`/`}>TODO</DropdownItem>*/}
-                  {/*          <Dropdown.Divider/>*/}
-                  {/*          <form onSubmit={null}>TODO*/}
-                  {/*            <button className='delete-schedule' type='submit'>TODO</button>*/}
-                  {/*          </form>*/}
-                  {/*        </DropdownMenu>*/}
-                  {/*      </Dropdown>*/}
-                  {/*    </td>*/}
-                  {/*  </tr>*/}
-                  {/*))}*/}
+                    {this.renderShows()}
                   </tbody>
                 </Table>
               </div>
